7235935c16cff4dd10a9d1e1aebd56e6e3646393
Your Name <ladraniful10@gmail.com>
2023-02-04 15:58:51 +0100

    build the backend

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/ErrorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+app.use(logger)
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname , '/public')))
+app.use('/' , require('./routes/root'))
+
+app.all('*' , (req,res)=>{
+  if(req.accepts('html')){
+    res.status(404)
+    .sendFile(path.join(__dirname  , 'views' , '404.html'))
+  }else if(req.accepts('json')){
+    res.json({message: '404 not found'})
+  }else{
+    res.type('txt').send("404 not found")
+  }
+})
+
+app.use(errorHandler)
+app.listen(PORT , ()=> console.log(`app running on port ${PORT}`))
